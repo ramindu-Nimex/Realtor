c606a3e8db0ef30f681f79407b2a578dad573316
Ramindu Nimesh <117975788+ramindu-Nimex@users.noreply.github.com>
2023-12-24 10:45:57 +0530

    Create private route and custom hook for protecting the profile page and add the logout functionality

---
+++ src/hooks/UseAuthStatus.jsx
@@ -0,0 +1,22 @@
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
+import { useEffect, useState } from 'react'
+
+export function UseAuthStatus() {
+   const [loggedIn, setLoggedIn] = useState(false)
+   const [checkingStatus, setCheckingStatus] = useState(true)
+
+   useEffect(() => {
+      const auth = getAuth()
+      onAuthStateChanged(auth, (user) => {
+         if (user) {
+            setLoggedIn(true)
+         } else {
+            setLoggedIn(false)
+         }
+         setCheckingStatus(false)
+      })
+   }, [])
+  return (
+    {loggedIn, checkingStatus}
+  )
+}
